8a2daa9dbdf97f736b30f2be8c326e568c3fab2a
Cheng-Dar Chiang <tboydar@gmail.com>
2018-12-03 17:33:03 +0800

    add write txt

--- dar1203.php
+++ dar1203.php
@@ -1,6 +1,15 @@
 <?php
 
-print("hello");
+$data = $_GET['command'];  // http://106.187.54.242/upload/dar1203.php?command=123
+
+$saveF = fopen("dar1203.txt","w");
+
+fwrite($saveF,$data);
+
+fclose($saveF);
+
+print($data);
 
 ?>
+
 
